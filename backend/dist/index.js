3fd5df7dffef67c413ae44955cdc7ff917af9d61
saikarthik04 <msksaikarthik2000@gmail.com>
2024-05-26 17:53:59 +0530

    initailized project

---
+++ backend/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const ws = new ws_1.WebSocketServer({ port: 8080 });
+ws.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+    ws.send('something');
+});
